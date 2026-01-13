083dfcfb2b5912eed894a9debcbabb672cf6306d
kirillaowens <workmailk3@gmail.com>
2026-01-13 12:34:36 +0300

    simple todo no mutation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  const count = useSelector((state) => state);
+  const todos = useSelector((state) => state);
   const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  const addToDo = () => {
+    if (!title.trim()) return;
+
+    dispatch({
+      type: "ADD",
+      title,
+    });
+
+    setTitle("");
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>{count}</p>
-        <button onClick={() => dispatch({ type: "DECREMENT" })}>-</button>
-        <button onClick={() => dispatch({ type: "INCREMENT" })}>+</button>
-        <button onClick={() => dispatch({ type: "RESET" })}>RESET</button>
+        <input
+          type="text"
+          placeholder="Введите задачу"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        {todos.map((todo) => (
+          <div className="todo" key={todo.id} style={{display: "flex"}}>
+            <p className="title">{todo.title}</p>
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={() => dispatch({ type: "TOGGLE", todoId: todo.id })}
+            />
+          </div>
+        ))}
+        <button onClick={addToDo}>Add</button>
+        <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
       </header>
     </div>
   );
--- src/store.js
+++ src/store.js
@@ -1,18 +1,40 @@
 import { legacy_createStore as createStore } from "redux";
 
-const initialState = 0;
+const initialState = [];
+let nextToDoId = 0;
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
+    case "ADD":
+      return [
+        ...state,
+        { title: action.title, id: ++nextToDoId, isCompleted: false },
+      ];
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.todoId
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo
+      );
     case "RESET":
       return initialState;
     default:
       return state;
   }
+};
+
+export const addToDo = (title) => {
+  return {
+    type: "ADD",
+    title,
+  };
+};
+
+export const toggleToDo = (todoId) => {
+  return {
+    type: "TOGGLE",
+    todoId,
+  };
 };
 
 const store = createStore(reducer);
